cf30cb9f54056dd00f7fd5a2b31743b2902d5d00
kylejohn06-py <Kingkylegumapit@gmail.com>
2024-08-30 18:26:49 +0800

    Create jumalon.py
    jumalon.py

---
+++ Activity 3/jumalon.py
@@ -0,0 +1,7 @@
+#write a program to prompt the user for hours and rate per hour to compute gross pay
+
+user1 = int(input("enter hours: "))
+user2 = float(input("enter rate: "))
+user3 = user1 * user2
+
+print("pay: ", str(user3))
