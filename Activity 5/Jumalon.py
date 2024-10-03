0192eb0b848e84cf22f1d4e60ebe60ba66a3a7f0
kylejohn06-py <Kingkylegumapit@gmail.com>
2024-10-03 09:02:45 +0800

    Jumalon.py

---
+++ Activity 5/Jumalon.py
@@ -0,0 +1,31 @@
+Character = input("Enter Text: ")
+
+upperC = 0
+lowerC = 0
+digitC = 0
+specialC = 0
+
+
+for character in string:
+    
+    if character.isupper():
+        
+        upperC = upperC + 1
+        
+    elif character.islower():
+        
+        lowerC = lowerC + 1
+        
+    elif character.isdigit():
+        
+        digitC = digitC + 1
+        
+    else:
+        
+        specialC = specialC + 1
+        
+
+print(f"UPPERCASE: {upperC}")
+print(f"LOWERCASE: {lowerC}")
+print(f"DIGIT: {digitC}")
+print(f"SPECIAL SYMBOLS: {specialC}")
